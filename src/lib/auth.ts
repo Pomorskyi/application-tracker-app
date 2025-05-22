e90f9e08d993d9f2d66ea1f9901e98416202329d
Misha Pomorskyi <mishapomorskyi@gmail.com>
2025-05-22 14:46:30 +0200

    Before deploy fixes 8

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,10 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export function verifyToken(token: string): { userId: number } | null {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("Missing JWT secret!");
+  }
+
   try {
     return jwt.verify(token, JWT_SECRET) as { userId: number };
   } catch {
@@ -11,5 +15,8 @@ export function verifyToken(token: string): { userId: number } | null {
 }
 
 export function generateToken(userId: number): string {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("Missing JWT secret!");
+  }
   return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: "7d" });
 }
